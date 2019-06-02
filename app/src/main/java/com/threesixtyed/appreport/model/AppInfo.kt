d1d33f211a1bd28b67f6b0caaaf9f4afa3bdb9ab
cosmas <cosmas259@gmail.com>
2019-06-02 17:17:00 +0630

    app report adapter

--- app/src/main/java/com/threesixtyed/appreport/model/AppInfo.kt
+++ app/src/main/java/com/threesixtyed/appreport/model/AppInfo.kt
@@ -1,4 +1,3 @@
 package com.threesixtyed.appreport.model
 
-class AppModel {
-}
+class AppInfo(var app_name:String,var img_url:String,var latest_vname:String,var latest_vlink:String)
---
+++ app/src/main/java/com/threesixtyed/appreport/model/AppReport.kt
@@ -0,0 +1,5 @@
+package com.threesixtyed.appreport.model
+
+data class AppReport(val android_version:String,val app_version:String,val phone_model:String,val report_date:String
+                    ,val report_detail:String,val report_type:String,val user_name:String) {
+}
