bd7f9b83c05bab6c751fa6e95a42e18894429343
Nay Ye Lin <nayye5154@gamil. om>
2019-06-05 14:06:47 +0630

    nyl update
    up

--- app/src/main/java/com/threesixtyed/appreport/LoginActivity.kt
+++ app/src/main/java/com/threesixtyed/appreport/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -27,6 +28,31 @@ class LoginActivity : AppCompatActivity() {
         sharePreferences=getSharedPreferences("mypref", Context.MODE_PRIVATE)
 
         databaseReference = FirebaseDatabase.getInstance().getReference("user")
+
+        //val snackbar = Snackbar.make(findViewById(R.id.lay), "Replace with your own action", Snackbar.LENGTH_LONG)
+
+/*
+        btnLogin.setOnClickListener {
+            var name: String = etName.text.toString()
+            var password: String = etPassword.text.toString()
+            var user=User(name,password)
+            databaseReference!!.push().key
+            databaseReference!!.child(databaseReference!!.push().key.toString()).setValue(user)
+
+
+            if (name.equals(""){
+
+           ) }
+            else if (password.equals(""){
+
+           ) }else{
+
+            }
+
+        }
+
+*/
+
 
         btnLogin.setOnClickListener {
             progressBar.visibility=View.VISIBLE
--- app/src/main/java/com/threesixtyed/appreport/fragment/AddProductFragment.kt
+++ app/src/main/java/com/threesixtyed/appreport/fragment/AddProductFragment.kt
@@ -3,14 +3,22 @@ package com.threesixtyed.appreport.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.database.*
 
 import com.threesixtyed.appreport.R
+import com.threesixtyed.appreport.model.AppInfo
+import com.threesixtyed.appreport.model.AppVersion
+import com.threesixtyed.appreport.model.User
+import kotlinx.android.synthetic.main.fragment_add_product.*
+import kotlinx.android.synthetic.main.fragment_adduser.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+ // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -18,14 +26,61 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
+
+
 class AddProductFragment : Fragment() {
+
+    var firebase:FirebaseDatabase?=null
+    var databaseRef:DatabaseReference?=null
+    var count:Long?=0
+    var appnameList:List<String>?=null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_product, container, false)
+        savedInstanceState: Bundle?): View? {
+        val view:View=inflater.inflate(R.layout.fragment_add_product, container, false)
+
+        databaseRef=FirebaseDatabase.getInstance().getReference().child("app")
+
+
+        val btn_upload=view.findViewById<Button>(R.id.btn_application_upload)
+        btn_upload.setOnClickListener{
+            Toast.makeText(context,count.toString(),Toast.LENGTH_LONG).show()
+
+            var app_name: String = et_application_name.text.toString()
+            var app_version: String = et_application_version.text.toString()
+            var app_link: String = et_application_link.text.toString()
+
+            if(app_name.isNotEmpty()&&app_version.isNotEmpty()&&app_link.isNotEmpty()){
+                //val key:String= databaseRef!!.push().key.toString()
+                databaseRef!!.child(app_name).setValue(app_name)
+                databaseRef!!.child(app_name).child("app_name").setValue(app_name)
+
+                databaseRef!!.child(app_name).child("latest_version").setValue(AppVersion(app_version,app_link,""))
+                databaseRef!!.child(app_name).child("version").child("1").setValue(AppVersion(app_version,app_link,""))
+
+                databaseRef!!.addValueEventListener(object :ValueEventListener{
+                    override fun onCancelled(p0: DatabaseError) {
+
+                    }
+
+                    override fun onDataChange(p0: DataSnapshot) {
+                        Toast.makeText(context,"Success",Toast.LENGTH_LONG).show()
+
+                    }
+
+                })
+            }else{
+                    et_application_name.setError("fill app Name")
+                    et_application_link.error="fill app link"
+                    et_application_version.setError("fill app version")
+            }
+
+
+        }
+        return view
+
     }
 
 
