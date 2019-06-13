8f1a8ceae8124e7902d715b18090858191346b55
Nay Zin Htoo <nayzinhtoo2018@gmail.com>
2019-06-13 11:18:34 +0630

    nzh

--- app/src/main/java/com/threesixtyed/appreport/ReportViewActivity.kt
+++ app/src/main/java/com/threesixtyed/appreport/ReportViewActivity.kt
@@ -76,55 +76,11 @@ class ReportViewActivity:AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
             R.id.report_filter -> {
-                filterDialog()
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun filterDialog() {
-        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this, R.style.customizedAlert)
 
-        var alertDialog=alertDialogBuilder.create()
-
-        val dialogView = LayoutInflater.from(this).inflate(R.layout.report_filter_dialog, null)
-
-        var btnFilter=dialogView.findViewById<Button>(R.id.btnFilter)
-        var btnFilterCancel=dialogView.findViewById<Button>(R.id.btnCancelFilter)
-        var sp_app_version=dialogView.findViewById<Spinner>(R.id.sp_app_version)
-        var sp_report_type=dialogView.findViewById<Spinner>(R.id.sp_report_type)
-        var ch_app_version=dialogView.findViewById<CheckBox>(R.id.ch_app_version)
-        var ch_report_type=dialogView.findViewById<CheckBox>(R.id.ch_report_type)
-
-        var versions = arrayOf("v1.0","v1.1","v2.0","v2.2.4","v3.0","v4.0","v5.0","v6.0","v6.0","v7.0")
-        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, versions)
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        sp_app_version!!.setAdapter(aa)
-
-        var reportType = arrayOf("Bug Report","Request Feature")
-        val bb = ArrayAdapter(this, android.R.layout.simple_spinner_item, reportType)
-        bb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        sp_report_type!!.setAdapter(bb)
-
-
-        btnFilter.setOnClickListener {
-            if(ch_app_version.isChecked)
-                Toast.makeText(this,"Selected version "+sp_app_version.selectedItem,Toast.LENGTH_LONG).show()
-            else
-                Toast.makeText(this,"Show all about version",Toast.LENGTH_LONG).show()
-            if(ch_report_type.isChecked)
-                Toast.makeText(this,"Selected Report Type "+sp_report_type.selectedItem,Toast.LENGTH_LONG).show()
-            else
-                Toast.makeText(this,"Show all about report Type",Toast.LENGTH_LONG).show()
-            alertDialog.dismiss()
-        }
-        btnFilterCancel.setOnClickListener {
-            alertDialog.dismiss()
-        }
-        alertDialog.setTitle("Filter Report")
-        alertDialog.setView(dialogView)
-        alertDialog.show()
-
-    }
 }
